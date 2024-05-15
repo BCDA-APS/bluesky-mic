5b71b456f11aefeb996df28832f0a3bcc7a8ea78
Yanqi Luo <yluo89@picobi438.xray.aps.anl.gov>
2024-05-15 16:47:54 -0500

    init commit

--- instrument/plans/__init__.py
+++ instrument/plans/__init__.py
@@ -2,5 +2,8 @@
 local, custom Bluesky plans (scans) and other functions
 """
 
+# from .quick_hello import *
+from .scan_record_2idd import *
+from .profile_move_isn import *
 # from .lup_plan import *
 # from .peak_finder_example import *
