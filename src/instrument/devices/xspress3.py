4c87410b6b94bee2510557ea9c114470e5cea8c7
Sector 2 Operations Account <user2idd@picobi438.xray.aps.anl.gov>
2024-10-07 18:48:15 -0500

    update xspress3 and fly2d plan

--- src/instrument/devices/xspress3.py
+++ src/instrument/devices/xspress3.py
@@ -49,8 +49,13 @@ class Xspress3(Device):
     def update_status(self):
         self.status = self.DetectorState_RBV.get(as_string=True)
 
+    def reset_capture_state(self):
+        if self.Capture_RBV.get():
+            yield from bps.mv(self.Capture, 0)
+            logger.info(f"Resetting xspress3's Capture state")
+
     def initialize(self, file_path=None, file_prefix=None, file_number=None,
-                      file_format="%s%s_%05d.h5"):
+                      file_format="%s%s_%05d.h5", trigger_mode=3):
         
         logger.info("Initialzing xspress3 hardware for XRF data collection")
         try:
@@ -71,42 +76,46 @@ class Xspress3(Device):
                 yield from bps.mv(self.FileNumber, file_number)
                 logger.info(f"Updating {self.FileNumber.pvname} to {file_number}")
             
-            yield from bps.mv(self.EraseOnStart, 0)
+            yield from bps.mv(
+                self.EraseOnStart, 0,
+                self.FileTemplate, file_format,
+                self.TriggerMode, trigger_mode)
+    
             logger.info(f"Updating {self.EraseOnStart.pvname} to 0")
-            yield from bps.mv(self.FileTemplate, file_format)
             logger.info(f"Updating {self.FileTemplate.pvname} to {file_format}")
+            logger.info(f"Updating {self.TriggerMode.pvname} to {trigger_mode}")
             self.update_status()
 
-    def setup_xspress3(self, npts, dwell_time, trigger_mode = 3):
+    def setup_xspress3(self, npts, dwell_time):
 
-        if all([self.status != "Idle"]):
-
-
-        if self.Capture_RBV.get():
-            yield from bps.mv(self.Capture, 1)
-            logger.info(f"Arming xspress3 {self.Capture.pvname} to {1}")
-        if self.Acquire.value==1:
+        self.update_status()
+        if not any([self.status == "Idle", self.status == "Aborted"]):
             yield from bps.mv(self.Acquire, 0)
-        if reset_counter: 
-            yield from bps.mv(self.FileNumber, 0)
+        if self.ArrayCounter_RBV.get() > 0:
+            yield from bps.mv(self.ERASE, 1)
 
-        yield from bps.mv(
-            self.ERASE, 1,
-            self.NumImages, npts,
-            self.AcquireTime, dwell_time,
-            self.EraseOnStart, 0,
-            self.TriggerMode, trigger_mode, 
-            self.EnableCallbacks, 1, 
-            self.AutoIncrement, 0,
-            self.FileNumber, scanNumber,
-            self.AutoSave, 1,
-            self.FileWriteMode, 2,
-            # self.FilePath, save_path,
-            # self.FileName, sample_name,
-            self.FileTemplate, f"%s%s_%05d.h5",
-            self.Capture, 1,
-            self.Acquire, 1, 
+        if self.status == "Idle":
+            yield from bps.mv(
+                self.NumImages, npts,
+                self.AcquireTime, dwell_time,
             )
+            logger.info(f"Updating {self.NumImages.pvname} to {npts}")
+            logger.info(f"Updating {self.AcquireTime.pvname} to {dwell_time}")
+
+            self.reset_capture_state()
+            self.reset_capture_state()
+
+            if not self.Capture_RBV.get():
+                yield from bps.mv(self.Capture, 1)
+                logger.info(f"Arming xspress3 changing {self.Capture.pvname} to {1}")
+                if self.Capture_RBV.get():
+                    yield from bps.mv(self.Acquire, 1)
+                logger.info(f"Xspress3 is ready to start")
+            else:
+                logger.error(f"Not able to arm the detector properly")
+        else:
+            logger.warning(f"Xspress3 not able to change state")
+
         
 pv = iconfig.get("DEVICES")["XSPRESS3"]
 xp3 = Xspress3(pv, name="xp3") 
