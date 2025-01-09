3e635931e4b1d7c94f2e37e1526e8c7e2913f62b
MDecarabas <72196105+MDecarabas@users.noreply.github.com>
2025-01-09 11:27:47 -0600

    table switched to progress text (#6)
    Co-authored-by: grace227 <yal227@eng.ucsd.edu>

--- src/instrument/plans/plan_blocks.py
+++ src/instrument/plans/plan_blocks.py
@@ -1,6 +1,7 @@
 from bluesky import preprocessors as bpp
 import bluesky.plan_stubs as bps
 from ophyd import Signal
+# logger = logging.getLogger(__name__)
 
 flag = Signal(name="flag", value=True)
 
@@ -20,11 +21,12 @@ def take_reading(counter):
 
 @bpp.run_decorator(md={})
 def count_subscriber(counter, setpoint):
-    # counter.subscribe(watch_counter)  # Collect a new event each time the scaler updates
+    counter.subscribe(watch_counter)  # Collect a new event each time the scaler updates
     while counter.value <= setpoint:
         # print("In counter_subscriber")
         if flag.get():
-            print(f"{counter=}")
+            # logger.info(f"{old_value=}, {value=}")
+            # print(f"{counter=}")
             yield from take_reading(counter)
             yield from bps.mv(flag, False)  # reset the flag
             if counter.value == setpoint:
