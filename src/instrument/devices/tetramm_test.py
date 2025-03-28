dbe40d83d3847b8b92248ecfde15417daffe4aac
grace227 <yal227@eng.ucsd.edu>
2025-03-28 13:55:00 -0500

    Included a very temporary implementation of a tetramm device that allows us to read the Ch1 current.
    This should be later substituted by the complete device.

--- src/instrument/devices/tetramm_test.py
+++ src/instrument/devices/tetramm_test.py
@@ -1,6 +1,6 @@
 from ophyd import Device, EpicsSignalRO, Component
 
 class Tetramm(Device):
-    current_1 = Component(EpicsSignalRO, 'Current1:MeanValue_RBV', name='current_1')
+    current_1 = Component(EpicsSignalRO, 'Current1:MeanValue_RBV', name='current_1', kind='hinted')
 
 tetramm_1 = Tetramm('19idSFT:TetrAMM1:', name='tetramm_1')
