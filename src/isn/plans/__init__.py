eb3794aed8e3228cd85ee8196262b7687ac12f6b
pdrmrcd <pdr.mrcd@gmail.com>
2025-09-30 11:21:31 -0500

    added logger to plans

--- src/isn/plans/__init__.py
+++ src/isn/plans/__init__.py
@@ -6,6 +6,7 @@
 # from .step2d import step2d 
 # from .step2d_random_pos import step2d_random_pos 
 from .flyscan import flyscan
+from .test_logger import test_logger
 # from .flyscan_eigerOnly import flyscan
 
 
