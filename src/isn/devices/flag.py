9571dada0204e8a5b636b7dad86f147229950db9
pdrmrcd <pdr.mrcd@gmail.com>
2025-09-30 11:15:55 -0500

    A standard Flag device implementation which can be used as detector when defining rois as well as some basic motor capabilities such as moving in and out of the beam position.

--- src/isn/devices/flag.py
+++ src/isn/devices/flag.py
@@ -1,10 +1,12 @@
-from ophyd.areadetector import DetectorBase
-from ophyd.areadetector import ADComponent
 from ophyd import FormattedComponent
-from ophyd import Component
 from ophyd import EpicsMotor
 
+from ophyd.areadetector import DetectorBase
 from ophyd.areadetector import CamBase
+from ophyd.areadetector import ROIPlugin
+from ophyd.areadetector import StatsPlugin
+
+from mic_common.devices.ad_fileplugin import MicHDF5
 
 from bluesky.plan_stubs import mv
 
@@ -12,12 +14,24 @@ class Flag(DetectorBase):
 
     _default_configuration_attrs = ()
 
-    cam = FormattedComponent(CamBase, "{cam_prefix}")
+
     motor = FormattedComponent(EpicsMotor, "{motor_prefix}")
+    cam = FormattedComponent(CamBase, "{flag_prefix}"+"cam1:")
+    hdf1 = FormattedComponent(MicHDF5, "{flag_prefix}"+"HDF1:")
+
+    roi1 = FormattedComponent(ROIPlugin, "{flag_prefix}"+"ROI1:")
+    roi2 = FormattedComponent(ROIPlugin, "{flag_prefix}"+"ROI2:")
+    roi3 = FormattedComponent(ROIPlugin, "{flag_prefix}"+"ROI3:")
+    roi4 = FormattedComponent(ROIPlugin, "{flag_prefix}"+"ROI4:")
+
+    stats1 = FormattedComponent(StatsPlugin, "{flag_prefix}"+"Stats1:")
+    stats2 = FormattedComponent(StatsPlugin, "{flag_prefix}"+"Stats2:")
+    stats3 = FormattedComponent(StatsPlugin, "{flag_prefix}"+"Stats3:")
+    stats4 = FormattedComponent(StatsPlugin, "{flag_prefix}"+"Stats4:")    
 
 
-    def __init__(self, cam_prefix, motor_prefix, in_position, out_position, *args, **kwargs):
-        self.cam_prefix = cam_prefix
+    def __init__(self, flag_prefix, motor_prefix, in_position, out_position, *args, **kwargs):
+        self.flag_prefix = flag_prefix
         self.motor_prefix = motor_prefix
         self._in_position = in_position
         self._out_position = out_position
@@ -26,10 +40,10 @@ class Flag(DetectorBase):
 
 
     def on(self):
-        self.cam.acquire.set(1)
+        self.cam.acquire.put(1)
 
     def off(self):
-        self.cam.acquire.set(0)
+        self.cam.acquire.put(0)
 
     def move_in(self):
         yield from mv(self.motor, self._in_position)
@@ -42,3 +56,9 @@ class Flag(DetectorBase):
             new_in_position = self.motor.user_readback.get()
 
         self._in_position = new_in_position
+
+    def set_out_position(self, new_out_position=None):
+        if not new_out_position:
+            new_out_position = self.motor.user_readback.get()
+
+        self._out_position = new_out_position
