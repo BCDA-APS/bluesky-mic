e062506b78be968f97f9ac6369181e842565d4ac
pdrmrcd <pdr.mrcd@gmail.com>
2025-10-04 18:08:51 -0500

    Added temporary patch for storage ring device

--- src/mic_common/devices/ring.py
+++ src/mic_common/devices/ring.py
@@ -1,2 +1,17 @@
-from apstools.devices.aps_machine import ApsMachineParametersDevice
+from apstools.devices.aps_cycle import ApsCycleDM
+from apstools.devices import ApsMachineParametersDevice
+from ophyd import Component
 
+class PatchedApsCycleDM(ApsCycleDM):
+    """BUGFIX for new fiscal year."""
+
+    _cycle_ends = "2025-12-31 23:59:59"  # TODO: official date in 2026-01
+    _cycle_name = "2025-3"  # TODO: apstools needs update
+
+    def get(self):
+        return self._cycle_name
+
+
+class PatchedApsMachineParametersDevice(ApsMachineParametersDevice):
+    """BUGFIX for new fiscal year."""
+    aps_cycle = Component(PatchedApsCycleDM)
