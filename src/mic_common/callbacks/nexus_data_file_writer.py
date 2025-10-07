b57c40b53e10aec0da4eab3ef94c6f9ee5efc88b
yal227 <yal227@eng.ucsd.edu>
2025-10-06 20:40:04 -0500

    update nexus file writer

--- src/mic_common/callbacks/nexus_data_file_writer.py
+++ src/mic_common/callbacks/nexus_data_file_writer.py
@@ -67,8 +67,9 @@ class MicNXWriter(NXWriter):
             path = self.file_path or pathlib.Path(".")
             return path / fname
         else:
-            self.savedata.update_next_file_name()
-            fname = self.savedata.next_file_name.replace(".mda", "_run.h5")
+            # self.savedata.update_next_file_name()
+            # fname = self.savedata.next_file_name.replace(".mda", "_run.h5")
+            fname = self.savedata.full_name.get().replace(".mda", "_run.h5")
             path = pathlib.Path(
                 self.savedata.get().file_system.replace("//micdata/data1", micdata_mountpath), "bluesky"
             )
