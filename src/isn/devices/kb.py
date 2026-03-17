b175e0d71740dc727b2571d8e3f909b94688a1ef
pdrmrcd <pdr.mrcd@gmail.com>
2026-03-17 10:33:04 -0500

    Created additional cap sensor motor classes to properly match ophyd component structure properly be able to save the piezo motor and coarse motor positions.

--- src/isn/devices/kb.py
+++ src/isn/devices/kb.py
@@ -14,9 +14,7 @@ class CapSensor(Device):
 
 
 class CapSensorMotor(EpicsMotor):
-    cap_sensor = FormattedComponent(CapSensor, "{_cap_sensor_pv}", name="cap_sensor")
-    coarse = None
-    fine = None
+    cap_sensor = FormattedComponent(CapSensor, "{self._cap_sensor_pv}")
 
     def __init__(
         self,
@@ -28,15 +26,21 @@ class CapSensorMotor(EpicsMotor):
         **kwargs,
     ):
         self._cap_sensor_pv = cap_sensor_pv
+        self._coarse_pv = coarse_pv
+        self._fine_pv = fine_pv
         super().__init__(positioner_pv, *args, **kwargs)
 
-        # TODO: Find a better way to implement this in case a motor doesn't have fine or coarse
-        if coarse_pv:
-            self._coarse_pv = coarse_pv
-            self.coarse = EpicsMotor(self._coarse_pv, name="coarse")
-        if fine_pv:
-            self._fine_pv = fine_pv
-            self.fine = EpicsMotor(self._fine_pv, name="fine")
+
+class CapSensorMotorCoarse(CapSensorMotor):
+    coarse = FormattedComponent(EpicsMotor, "{self._coarse_pv}")
+
+
+class CapSensorMotorFine(CapSensorMotor):
+    fine = FormattedComponent(EpicsMotor, "{self._fine_pv}")
+
+
+class CapSensorMotorCoarseFine(CapSensorMotorCoarse, CapSensorMotorFine):
+    pass
 
 
 class KB(Device):
@@ -52,7 +56,7 @@ class KB(Device):
     )
 
     x = Component(
-        CapSensorMotor,
+        CapSensorMotorCoarseFine,
         positioner_pv="19idKB:m18",
         cap_sensor_pv="19idKB:cap4:",
         coarse_pv="19idKB:m12",
@@ -61,7 +65,7 @@ class KB(Device):
     )
 
     y_ds = Component(
-        CapSensorMotor,
+        CapSensorMotorCoarseFine,
         positioner_pv="19idKB:m16",
         cap_sensor_pv="19idKB:cap2:",
         coarse_pv="19idKB:m11",
@@ -70,7 +74,7 @@ class KB(Device):
     )
 
     y_us = Component(
-        CapSensorMotor,
+        CapSensorMotorCoarseFine,
         positioner_pv="19idKB:m15",
         cap_sensor_pv="19idKB:cap1:",
         coarse_pv="19idKB:m7",
@@ -79,7 +83,7 @@ class KB(Device):
     )
 
     z = Component(
-        CapSensorMotor,
+        CapSensorMotorCoarseFine,
         positioner_pv="19idKB:m17",
         cap_sensor_pv="19idKB:cap3:",
         coarse_pv="19idKB:m8",
@@ -88,7 +92,7 @@ class KB(Device):
     )
 
     theta_y = Component(
-        CapSensorMotor,
+        CapSensorMotorCoarseFine,
         positioner_pv="19idKB:SM1",
         cap_sensor_pv="19idKB:cap5:",
         coarse_pv="19idKB:m9",
@@ -97,7 +101,7 @@ class KB(Device):
     )
 
     theta_z = Component(
-        CapSensorMotor,
+        CapSensorMotorCoarse,
         positioner_pv="19idKB:SM2",
         cap_sensor_pv="19idKB:cap6:",
         coarse_pv="19idKB:m10",
@@ -107,18 +111,3 @@ class KB(Device):
     vertical_angle = Component(EpicsSignalRO, "vert:t2.C", name="vertical")
     vertical_average = Component(EpicsSignalRO, "vert:t2.D", name="vertical")
 
-    # def __init__(self, prefix, *args, **kwargs):
-    #     super().__init__(prefix, *args, **kwargs)
-
-
-# class KB(Device):
-#     # th_x = Component(EpicsMotor, ':m9', kind='config', labels=('motor',)) #Need to think right way of implementing it
-#     th_y = Component(EpicsMotor, ':SM1', kind='config', labels=('motor','baseline'))
-#     th_y_cap = Component(EpicsSignal, ':cap5:voltage', kind='config', labels=('baseline'))
-#     th_z = Component(EpicsMotor, ':SM2', kind='config', labels=('motor','baseline'))
-#     th_z_cap = Component(EpicsSignal, ':cap6:voltage', kind='config', labels=('baseline'))
-#     x = Component(EpicsMotor, ':m18', kind='config', labels=('motor',))
-#     x_cap = Component(EpicsSignal, ':cap4:voltage', kind='config', labels=('baseline'))
-#     # y = Component(EpicsMotor, ':m9', kind='config', labels=('motor',)) #Need to think how to do it properly
-#     z = Component(EpicsMotor, ':m17', kind='config', labels=('motor',))
-#     z_cap = Component(EpicsSignal, ':cap3:voltage', kind='config', labels=('baseline'))
