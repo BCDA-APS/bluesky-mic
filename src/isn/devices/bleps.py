1b6ded7b248d63a04c6d2a6f59f7a3e7462cd275
pdrmrcd <pdr.mrcd@gmail.com>
2026-09-15 16:36:22 -0500

    Included the QPC ion pumps in bleps for logging measurements

--- src/isn/devices/bleps.py
+++ src/isn/devices/bleps.py
@@ -1,11 +1,22 @@
 from ophyd import Device
 from ophyd import Component
+from ophyd import FormattedComponent
 from ophyd import EpicsSignalRO
 
-class Bleps(Device):
-    """Class for the BLEPS monitoring"""
+class QpcIonPump(Device):
 
-    _defaul_read_attrs = ['temp1',
+    _default_read_attrs = ['pressure',
+                           'current',
+                ]
+
+    pressure = Component(EpicsSignalRO, 'Pressure', name='pressure', kind='normal')
+    current = Component(EpicsSignalRO, 'Current', name='current', kind='normal')
+    voltage = Component(EpicsSignalRO, 'Voltage', name='voltage', kind='normal')
+
+class Bleps(Device):
+    """Class for the BLEPS Temperatures monitoring"""
+
+    _default_read_attrs = ['temp1',
                            'temp2',
                            'temp3',
                            'temp4',
@@ -26,27 +37,59 @@ class Bleps(Device):
                            'temp19',
                            'temp20',
                            'temp21',
-                           'temp22']
+                           'temp22',
+                           'pump1',
+                           'pump2',
+                           'pump3',
+                           'pump4',
+                           'pump5',
+                           'pump12',
+                           'pump13',
+                           ]
 
-    temp1 = Component(EpicsSignalRO, 'TEMP1_CURRENT', name='M12-LatX-Mt', kind='normal')
-    temp2 = Component(EpicsSignalRO, 'TEMP2_CURRENT', name='M12-Bend1-Mt', kind='normal')
-    temp3 = Component(EpicsSignalRO, 'TEMP3_CURRENT', name='M12-Bend2-Mt', kind='normal')
-    temp4 = Component(EpicsSignalRO, 'TEMP4_CURRENT', name='M1-Mask', kind='normal')
-    temp5 = Component(EpicsSignalRO, 'TEMP5_CURRENT', name='M2-Mask', kind='normal')
-    temp6 = Component(EpicsSignalRO, 'TEMP6_CURRENT', name='HDCM-Bragg-Mt', kind='normal')
-    temp7 = Component(EpicsSignalRO, 'TEMP7_CURRENT', name='HDCM-Gap-Mt', kind='normal')
-    temp8 = Component(EpicsSignalRO, 'TEMP8_CURRENT', name='HDCM-Pitch-Mt', kind='normal')
-    temp9 = Component(EpicsSignalRO, 'TEMP9_CURRENT', name='HDCM-Roll-Mt', kind='normal')
-    temp10 = Component(EpicsSignalRO, 'TEMP10_CURRENT', name='HDCM-1st-Xtal-Bott', kind='normal')
-    temp11 = Component(EpicsSignalRO, 'TEMP11_CURRENT', name='HDCM-2nd-Xtal-Bott', kind='normal')
-    temp12 = Component(EpicsSignalRO, 'TEMP12_CURRENT', name='HDCM-Xtal-Mask', kind='normal')
-    temp13 = Component(EpicsSignalRO, 'TEMP13_CURRENT', name='M3-Pitch-Mt', kind='normal')
-    temp14 = Component(EpicsSignalRO, 'TEMP14_CURRENT', name='M3-Bend1-Mt', kind='normal')
-    temp15 = Component(EpicsSignalRO, 'TEMP15_CURRENT', name='M3-Bend2-Mt', kind='normal')
-    temp16 = Component(EpicsSignalRO, 'TEMP16_CURRENT', name='M3-Mask', kind='normal')
-    temp17 = Component(EpicsSignalRO, 'TEMP17_CURRENT', name='M1-Inner-Blade', kind='normal')
-    temp18 = Component(EpicsSignalRO, 'TEMP18_CURRENT', name='M1-Outer-Blade', kind='normal')
-    temp19 = Component(EpicsSignalRO, 'TEMP19_CURRENT', name='M2-Inner-Blade', kind='normal')
-    temp20 = Component(EpicsSignalRO, 'TEMP20_CURRENT', name='M2-Outer-Blade', kind='normal')
-    temp21 = Component(EpicsSignalRO, 'TEMP21_CURRENT', name='M3-Upper-Blade', kind='normal')
-    temp22 = Component(EpicsSignalRO, 'TEMP22_CURRENT', name='M3-Lower-Blade', kind='normal')
+    temp1 = FormattedComponent(EpicsSignalRO, "{temps_prefix}"+"TEMP1_CURRENT", name='M12-LatX-Mt', kind='normal')
+    temp2 = FormattedComponent(EpicsSignalRO, "{temps_prefix}"+ "TEMP2_CURRENT", name='M12-Bend1-Mt', kind='normal')
+    temp3 = FormattedComponent(EpicsSignalRO, "{temps_prefix}"+ "TEMP3_CURRENT", name='M12-Bend2-Mt', kind='normal')
+    temp4 = FormattedComponent(EpicsSignalRO, "{temps_prefix}"+ "TEMP4_CURRENT", name='M1-Mask', kind='normal')
+    temp5 = FormattedComponent(EpicsSignalRO, "{temps_prefix}"+ "TEMP5_CURRENT", name='M2-Mask', kind='normal')
+    temp6 = FormattedComponent(EpicsSignalRO, "{temps_prefix}"+ "TEMP6_CURRENT", name='HDCM-Bragg-Mt', kind='normal')
+    temp7 = FormattedComponent(EpicsSignalRO, "{temps_prefix}"+ "TEMP7_CURRENT", name='HDCM-Gap-Mt', kind='normal')
+    temp8 = FormattedComponent(EpicsSignalRO, "{temps_prefix}"+ "TEMP8_CURRENT", name='HDCM-Pitch-Mt', kind='normal')
+    temp9 = FormattedComponent(EpicsSignalRO, "{temps_prefix}"+ "TEMP9_CURRENT", name='HDCM-Roll-Mt', kind='normal')
+    temp10 = FormattedComponent(EpicsSignalRO, "{temps_prefix}"+ "TEMP10_CURRENT", name='HDCM-1st-Xtal-Bott', kind='normal')
+    temp11 = FormattedComponent(EpicsSignalRO, "{temps_prefix}"+ "TEMP11_CURRENT", name='HDCM-2nd-Xtal-Bott', kind='normal')
+    temp12 = FormattedComponent(EpicsSignalRO, "{temps_prefix}"+ "TEMP12_CURRENT", name='HDCM-Xtal-Mask', kind='normal')
+    temp13 = FormattedComponent(EpicsSignalRO, "{temps_prefix}"+ "TEMP13_CURRENT", name='M3-Pitch-Mt', kind='normal')
+    temp14 = FormattedComponent(EpicsSignalRO, "{temps_prefix}"+ "TEMP14_CURRENT", name='M3-Bend1-Mt', kind='normal')
+    temp15 = FormattedComponent(EpicsSignalRO, "{temps_prefix}"+ "TEMP15_CURRENT", name='M3-Bend2-Mt', kind='normal')
+    temp16 = FormattedComponent(EpicsSignalRO, "{temps_prefix}"+ "TEMP16_CURRENT", name='M3-Mask', kind='normal')
+    temp17 = FormattedComponent(EpicsSignalRO, "{temps_prefix}"+ "TEMP17_CURRENT", name='M1-Inner-Blade', kind='normal')
+    temp18 = FormattedComponent(EpicsSignalRO, "{temps_prefix}"+ "TEMP18_CURRENT", name='M1-Outer-Blade', kind='normal')
+    temp19 = FormattedComponent(EpicsSignalRO, "{temps_prefix}"+ "TEMP19_CURRENT", name='M2-Inner-Blade', kind='normal')
+    temp20 = FormattedComponent(EpicsSignalRO, "{temps_prefix}"+ "TEMP20_CURRENT", name='M2-Outer-Blade', kind='normal')
+    temp21 = FormattedComponent(EpicsSignalRO, "{temps_prefix}"+ "TEMP21_CURRENT", name='M3-Upper-Blade', kind='normal')
+    temp22 = FormattedComponent(EpicsSignalRO, "{temps_prefix}"+ "TEMP22_CURRENT", name='M3-Lower-Blade', kind='normal')
+
+    pump1 = FormattedComponent(QpcIonPump, "{pump_prefix}" + 'qpc01_1:', name='WBS', kind='normal')
+    pump2 = FormattedComponent(QpcIonPump, "{pump_prefix}" + 'qpc01_2:', name='MR1-2', kind='normal')
+    pump3 = FormattedComponent(QpcIonPump, "{pump_prefix}" + 'qpc01_3:', name='PBS', kind='normal')
+    pump4 = FormattedComponent(QpcIonPump, "{pump_prefix}" + 'qpc01_4:', name='Mono', kind='normal')
+    pump5 = FormattedComponent(QpcIonPump, "{pump_prefix}" + 'qpc02_1:', name='MR3', kind='normal')
+    pump6 = FormattedComponent(QpcIonPump, "{pump_prefix}" + 'qpc02_2:', name='SafetyShutter', kind='normal')
+    pump7 = FormattedComponent(QpcIonPump, "{pump_prefix}" + 'qpc02_3:', name='Flag2', kind='normal')
+    pump8 = FormattedComponent(QpcIonPump, "{pump_prefix}" + 'qpc02_4:', name='BPM1-US', kind='normal')
+    pump9 = FormattedComponent(QpcIonPump, "{pump_prefix}" + 'qpc03_1:', name='BPM1-DS', kind='normal')
+    pump10 = FormattedComponent(QpcIonPump, "{pump_prefix}" + 'qpc04_1:', name='BPM2-US', kind='normal')
+    pump11 = FormattedComponent(QpcIonPump, "{pump_prefix}" + 'qpc04_2:', name='BPM2-DS', kind='normal')
+    pump12 = FormattedComponent(QpcIonPump, "{pump_prefix}" + 'qpc04_3:', name='Transport1', kind='normal')
+    pump13 = FormattedComponent(QpcIonPump, "{pump_prefix}" + 'qpc04_4:', name='Transport2', kind='normal')
+    pump14 = FormattedComponent(QpcIonPump, "{pump_prefix}" + 'qpc05_1:', name='IP14', kind='normal')
+    pump15 = FormattedComponent(QpcIonPump, "{pump_prefix}" + 'qpc05_2:', name='IP15', kind='normal')
+    pump16 = FormattedComponent(QpcIonPump, "{pump_prefix}" + 'qpc05_3:', name='IP16', kind='normal')
+    pump17 = FormattedComponent(QpcIonPump, "{pump_prefix}" + 'qpc05_4:', name='IP17', kind='normal')
+
+
+    def __init__(self, pump_prefix, temps_prefix, *args, **kwargs):
+        self.pump_prefix = pump_prefix
+        self.temps_prefix = temps_prefix
+        super().__init__(*args, **kwargs)
