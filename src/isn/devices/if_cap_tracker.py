cfa8d3f54d3d42e54ffb9be44bd6a7ad17a5a631
pdrmrcd <pdr.mrcd@gmail.com>
2026-09-15 16:54:01 -0500

    Extended the if_cap_tracker device to measure the sample position based on the interferometry readings. Next time we have x, y, z at 0 value, we can reset the interferometry readings to ensure that these agree.

--- src/isn/devices/if_cap_tracker.py
+++ src/isn/devices/if_cap_tracker.py
@@ -1,6 +1,7 @@
 from collections import OrderedDict
 
 from ophyd import Device
+from ophyd import Signal
 from ophyd import Component
 from ophyd import EpicsSignal
 from ophyd import EpicsSignalRO
@@ -52,14 +53,23 @@ class IfCapTracker(Device):
     _default_read_attrs = ['caps', 
                            'if_tracker_1', 
                            'if_tracker_2', 
-                           'if_tracker_3']
+                           'if_tracker_3',
+                           'x',
+                           'y',
+                           'z']
     
     caps = DynamicDeviceComponent(_cap_fields())
     acq_start = Component(EpicsSignal, ':SG:ADC_START_CVT_Signal')
 
+    buffer1 = Component(EpicsSignal, ':SG:BUFFER-1_N_Signal', kind='config')
+
     if_tracker_1 = DynamicDeviceComponent(_interferometer_tracker(1))
     if_tracker_2 = DynamicDeviceComponent(_interferometer_tracker(2))
     if_tracker_3 = DynamicDeviceComponent(_interferometer_tracker(3, num=3))
+
+    x = Component(Signal, name='x', kind='hinted', value=0)
+    y = Component(Signal, name='y', kind='hinted', value=0)
+    z = Component(Signal, name='z', kind='hinted', value=0)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -67,7 +77,26 @@ class IfCapTracker(Device):
 
     def trigger(self):
         return self.acq_start.set('1!')
-    
+
+    def calc_position(self, vals: dict):
+        x1 = vals['if_cap_tracker_if_tracker_3_if3']['value'] # 'I15 (X)'
+        x2 = vals['if_cap_tracker_if_tracker_2_if4']['value'] # 'I10 (X-us)'
+        x3 = vals['if_cap_tracker_if_tracker_2_if5']['value'] # 'I11 (X-ds)'
+        y1 = vals['if_cap_tracker_if_tracker_2_if1']['value'] # 'I7 (Y ds)'
+        y2 = vals['if_cap_tracker_if_tracker_2_if2']['value'] # 'I8 (Y us-ob)'
+        y3 = vals['if_cap_tracker_if_tracker_2_if3']['value'] # 'I9 (Y us-ib)'
+        z = vals['if_cap_tracker_if_tracker_2_if6']['value'] # 'I12 (Z)'
+
+        x = round((x1 + x2 + x3) / 3, 7)
+        y = round((y1 + y2 + y3) / 3, 7)
+
+        vals[self.x.name] = {'value': x, 'timestamp': vals['if_cap_tracker_if_tracker_3_if3']['timestamp']}
+        vals[self.y.name] = {'value': y, 'timestamp': vals['if_cap_tracker_if_tracker_2_if1']['timestamp']}
+        vals[self.z.name] = {'value': z, 'timestamp': vals['if_cap_tracker_if_tracker_2_if6']['timestamp']}
+
+        return vals
+
+
     def read(self):
         vals =  super().read()
         for key in vals:
@@ -75,16 +104,26 @@ class IfCapTracker(Device):
                 vals[key]['value'] = bits_to_mm(vals[key]['value'])
             elif key.startswith('if_cap_tracker_if_tracker'):
                 vals[key]['value'] = interf_to_mm(vals[key]['value'])
+
+        vals = self.calc_position(vals)
+        for sig in (self.x, self.y, self.z):
+            sig.put(vals[sig.name]['value'])
         return vals
-    
+
     def describe(self):
         desc = super().describe()
         new_desc = OrderedDict()
+        xyz_keys = {self.x.name, self.y.name, self.z.name}
         for key, entry in desc.items():
             new_entry = dict(entry)
-            if key.startswith('if_cap_tracker_caps') or key.startswith('if_cap_tracker_if_tracker'):
+            if key.startswith('if_cap_tracker_caps') or \
+                    key.startswith('if_cap_tracker_if_tracker') or \
+                    key in xyz_keys:
                 new_entry['dtype'] = 'number'
                 # Optionally update units:
                 new_entry['units'] = 'mm'
             new_desc[key] = new_entry
         return new_desc
+
+    def reset(self):
+        self.buffer1.set("1!").wait()
